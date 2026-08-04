33d8b2db8b4945d7daed1994e4512ec11023fe38
ashoang-splunk <ahoang@splunk.com>
2026-08-04 13:39:03 -0400

    Added capacitor items and update CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,13 +58,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://192.168.4.52:8080",
+    "capacitor://localhost",
 ]
 
 ROOT_URLCONF = 'config.urls'
--- frontend/src/api/server-consts.ts
+++ frontend/src/api/server-consts.ts
@@ -1,2 +1,4 @@
 // export const API_BASE_URL = "http://127.0.0.1:8000/api";
-export const API_BASE_URL = "http://192.168.4.52:8000/api";
+// export const API_BASE_URL = "http://192.168.4.52:8000/api";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log("API_BASE_URL:", API_BASE_URL);
